abf7898cbd680814d6fce02634ec7f49772e34c6
Nikita Baranov <nekitkass@gmail.com>
2024-09-03 16:45:55 +0300

    add gitignore

---
+++ src/constants/constants.ts
@@ -0,0 +1,10 @@
+export const ACCESS_TOKEN_EXPIRY = {
+  millisecond: 60000 * 100,
+  seconds: 60 * 100,
+  minutes: '10m'
+};
+export const REFRESH_TOKEN_EXPIRY = {
+  millisecond: 180000 * 100,
+  seconds: 180 * 100,
+  minutes: '30m'
+};
